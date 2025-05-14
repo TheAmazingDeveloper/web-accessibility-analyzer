c1769b2a61a8e61e93fcd8335518692b2b937f78
Darshit Jain <darshitjain09092006@gmail.com>
2025-05-14 15:17:01 +0530

    Project Setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const inter = Inter({ variable: "--font-inter", subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Accessible",
+  description:
+    "Discover the Web Accessibility Analyzer, a powerful tool built with Next.js, Tailwind CSS, and Axe Core. This analyzer evaluates website accessibility by generating a comprehensive score and identifying issues classified as minor, moderate, or critical. Simply enter a URL to get actionable insights that help make your site more user-friendly and inclusive.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
